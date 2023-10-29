d9e6318be798e5d60369642544cbf54ffe332a0a
Karl Hiner <karl.hiner@gmail.com>
2023-10-29 14:45:09 -0400

    Redraw the scene every frame the window size changes. Lots of Vulkan perf/resource improvements/cleanup (below):
    - Use ImGui window bg color for scene bg.
    - Use the scene ImGui window size instead of the SDL window size to draw triangle scene.
    - Reuse sampler across renders
    - Reuse command buffer across renders using `eResetCommandBuffer` flag
    - Reuse the graphics pipeline across renders using dynamic viewport and scissor states
    - Remove unnecessary subpass dependency
    - Use more vector/array create_info args
    - Remove some state members from `TriangleContext` that are only needed once

--- src/Scene.h
+++ src/Scene.h
@@ -3,33 +3,25 @@
 #include "VulkanContext.h"
 
 struct Scene {
-    Scene(const VulkanContext &, uint width, uint height);
+    Scene(const VulkanContext &);
     ~Scene() = default; // Using unique handles, so no need to manually destroy anything.
 
-    struct TriangleContext {
-        vk::UniqueShaderModule VertexShaderModule;
-        vk::UniqueShaderModule FragmentShaderModule;
-        vk::UniquePipelineLayout PipelineLayout;
-        vk::UniquePipeline GraphicsPipeline;
-        vk::UniqueRenderPass RenderPass;
-        vk::UniqueCommandPool CommandPool;
-        vk::UniqueFramebuffer Framebuffer; // Single framebuffer for offscreen rendering.
-        std::vector<vk::UniqueCommandBuffer> CommandBuffers;
+    // Returns true if the scene was updated.
+    bool Render(uint width, uint height, const vk::ClearColorValue &bg_color);
 
+    struct TriangleContext {
+        vk::SampleCountFlagBits MsaaSamples;
+        vk::UniqueRenderPass RenderPass;
+        vk::UniquePipeline GraphicsPipeline;
+        vk::UniqueCommandPool CommandPool;
+        std::vector<vk::UniqueCommandBuffer> CommandBuffers;
+        vk::UniqueSampler TextureSampler;
         vk::Extent2D Extent;
 
-        // The scene is rendered to this image.
-        vk::UniqueImage OffscreenImage;
-        vk::UniqueImageView OffscreenImageView;
-        vk::UniqueDeviceMemory OffscreenImageMemory;
-
-        // The image is resolved to this image with MSAA.
+        // The scene is rendered to an offscreen image and then resolved to this image using MSAA.
         vk::UniqueImage ResolveImage;
         vk::UniqueImageView ResolveImageView;
         vk::UniqueDeviceMemory ResolveImageMemory;
-
-        vk::UniqueSampler TextureSampler;
-        vk::DescriptorSet DescriptorSet; // Not unique, since this is returned by `ImGui_ImplVulkan_AddTexture` as a `VkDescriptorSet`.
     };
 
     const VulkanContext &VC;
